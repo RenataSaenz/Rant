d3320dcb2466b8231be26841f27f0d1be1c4c8bb
LuciaNicolau <70971261+LuciaNicolau@users.noreply.github.com>
2021-09-10 17:04:55 -0300

    Collectables and pause

---
+++ Assets/Script/Control.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Control 
+{
+    private Ant ant;
+    
+
+    Action controlsMethod;
+
+    public Control(Ant controller)
+    {
+        ant = controller;
+        controlsMethod = NormalControls;
+    }
+
+    public void OnUpdate()
+    {
+        controlsMethod();
+    }
+
+    void NormalControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("Pausa");
+            Time.timeScale = 0f;
+            controlsMethod = PausedControls;
+            ant.managerUI.ActivePause();
+        }
+
+    }
+
+    void PausedControls()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Time.timeScale = 1f;
+            controlsMethod = NormalControls;
+            ant.managerUI.InactivePause();
+        }
+
+    }
+}
---
+++ Assets/Script/ManagerUI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ManagerUI : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public GameObject pausedMenu;
+    public Image cameraShutDown;
+    public float speedFade;
+
+    private void Start()
+    {
+        EventManager.Subscribe("GameOver", FadeOn);
+        cameraShutDown.canvasRenderer.SetAlpha(0f);
+    }
+
+    public void ChangeScore(int score)
+    {
+        text.text = "SCORE: " + score.ToString();
+    }
+
+    public void ActivePause()
+    {
+        pausedMenu.SetActive(true);
+    }
+
+    public void InactivePause()
+    {
+        pausedMenu.SetActive(false);
+    }
+
+    public void FadeOn(params object[] parameters)
+    {
+        StartCoroutine(FadeActive());
+    }
+
+    IEnumerator FadeActive()
+    {
+        Color fade = cameraShutDown.color;
+        fade.a = 1;
+
+        while (cameraShutDown.color.a < 1)
+        {
+            cameraShutDown.color = Color.Lerp(cameraShutDown.color, fade, speedFade * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
