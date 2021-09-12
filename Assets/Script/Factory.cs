4e1ac52f2dfeb9f74954a834a06fd0c18b73988c
RenataSaenz <renata.saenz@davinci.edu.ar>
2021-09-11 23:12:52 -0300

    Pool and Factory

---
+++ Assets/Script/Factory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creo dos factory:
+//Uno no requiere ningun prefab y el otro tiene dos tipos de generics.
+
+public interface IFactory<T>
+{
+    T Create(T parameter);
+}
+
+public interface IFactory<T, P>
+{
+    T Create(P parameter);
+}
+public class FoodFactory : IFactory<GameObject>
+{
+    public GameObject Create(GameObject foodPrefab)
+    {
+        var food = Object.Instantiate(foodPrefab);
+        return food;
+    }
+}
---
+++ Assets/Script/FoodManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManager : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    //private FoodFactory _factory;
+  //  private IFactory<GameObject, GameObject> _factory = new FoodFactory();
+
+    private void Awake()
+    {
+        var pool = new Pool<Food>(Create, 5);
+    }
+
+    private Food Create()
+    {
+        PlaceFood();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlaceFood();
+        }
+    }
+    private void PlaceFood() {
+        FoodFactory _factory = new FoodFactory();
+        var food = _factory.Create(foodPrefab);
+    }
+
+}
