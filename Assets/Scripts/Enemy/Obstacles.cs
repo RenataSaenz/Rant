3d7b0ab5f69bd6e932de306ec55d6060d3497a20
LuciaNicolau <70971261+LuciaNicolau@users.noreply.github.com>
2021-10-03 16:35:08 -0300

    Cambios 3

---
+++ Assets/Scripts/Enemy/Obstacles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    public int damage;
+    public Obstacles SetPosition(Transform t)
+    {
+        transform.position = t.position;
+        return this;
+    }
+    
+    public Obstacles SetScale(float x = 1, float y = 1, float z = 1)
+    {
+        transform.localScale = new Vector3(x, y, z);
+        return this;
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        var damageable = trigger.GetComponent<IDamageable>();
+        if (damageable != null)
+        {
+            damageable.SubtractLifeFunc(damage);
+        }
+        
+    }
+}
