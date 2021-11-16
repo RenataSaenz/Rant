548c99e7a3f327860a91f405ecaf191fbc14c61e
LuciaNicolau <70971261+LuciaNicolau@users.noreply.github.com>
2021-11-16 10:26:08 -0300

    Ads scripts

---
+++ Assets/Scripts/Ads/AdsInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInitializer : MonoBehaviour
+{
+    public string idIOS = "4447118";
+    public string idANDROID = "4447119";
+    private void Awake()
+    {
+#if UNITY_IOS
+            Advertisement.Initialize(idIOS);
+#endif
+#if UNITY_ANDROID
+            Advertisement.Initialize(idANDROID, true);
+#endif
+    }
+}
---
+++ Assets/Scripts/Ads/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public RewardedAds rewardedAds;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            rewardedAds.LoadAd();
+    }
+}
