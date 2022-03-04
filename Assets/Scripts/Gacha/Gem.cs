32f05289bc3a1a5b1a50f5613316c8228a9b0b68
RenataSaenz <71044498+RenataSaenz@users.noreply.github.com>
2022-03-04 04:31:01 -0300

    cambios

---
+++ Assets/Scripts/Gacha/Gem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour, ICollectable
+{
+  public void Collect()
+  {
+    GemsContoller.SumGems(1);
+    gameObject.SetActive(false);
+    SoundManager.instance.Play(SoundManager.Types.Victory);
+  }
+}
